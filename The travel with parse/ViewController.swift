5149eb5582483f9ef34bb8614a4cb53ef9cb9016
johnny7861532 <johnny7861532@hotmail.com>
2016-04-07 13:57:19 +0800

    Initial Commit

---
+++ The travel with parse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The travel with parse
+//
+//  Created by Johnny' mac on 2016/4/7.
+//  Copyright © 2016年 Johnny' mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
